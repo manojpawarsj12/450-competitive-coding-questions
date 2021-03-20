9bea788874421f9130fc17381e34720a716ebcfc
manojpawarsj12 <manojpawarsj.mp11@gmail.com>
2021-03-20 12:42:24 +0530

    2

---
+++ 2_max_min_ele_array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[100], n, temp;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int min = arr[0];
+    int max = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    cout << min << max << endl;
+    return 0;
+}
